780e500277b82138d61508ae524b61e0353bfe5a
KUAKAKA <elik@example.com>
2021-04-26 17:22:02 +0300

    Structure is done

--- main.c
+++ main.c
@@ -8,8 +8,8 @@ int contains(const char *array, char chr);
 
 void createGraph(char *inputFile, char *outputFile);
 
-void addVertexAndEdgeToGraph(char ***adjacencyList, char **vertices, int **edgesCount, int *op,  const char *vertex,
-                              const char *edge);
+void addVertexAndEdgeToGraph(char ***adjacencyList, char **vertices, int **edgesCount, int *op, const char *vertex,
+                             const char *edge);
 
 typedef struct Graph {
     char **adjacencyList;
@@ -17,8 +17,12 @@ typedef struct Graph {
     int *edgesCount;
 } Graph;
 
-int main(void) {
-    createGraph("C:/5th/Graph.txt", "C:/5th/out.txt");
+int main(int argc, char *args[]) {
+    if (argc != 3) {
+        printf("IllegalArgumentException. InputFileName OutputFileName");
+        exit(1);
+    }
+    createGraph(args[1], args[2]);
     return EXIT_SUCCESS;
 }
 
@@ -29,9 +33,6 @@ void createGraph(char *inputFile, char *outputFile) {
     graph.edgesCount = NULL;
     int order = 0;//индекс прохода по массивам vertices и edgesCount.
     char vertex, edge;
-    char *vp = &vertex;
-    char *ep = &edge;
-    int *op = &order;
     FILE *fp;
 
     if ((fp = fopen(inputFile, "r")) == NULL) {
@@ -39,8 +40,9 @@ void createGraph(char *inputFile, char *outputFile) {
         exit(1);
     }
 
-    while (fscanf(fp, "%c-%c\n", vp, ep) == 2) {
-        addVertexAndEdgeToGraph(&graph.adjacencyList, &graph.vertices, &graph.edgesCount, op, vp, ep);
+    while (fscanf(fp, "%c-%c\n", &vertex, &edge) == 2) {
+        addVertexAndEdgeToGraph(&graph.adjacencyList, &graph.vertices, &graph.edgesCount, &order, &vertex, &edge);
+        addVertexAndEdgeToGraph(&graph.adjacencyList, &graph.vertices, &graph.edgesCount, &order, &edge, &vertex);
     }
     if ((fp = fopen(outputFile, "w")) == NULL) {
         printf("Fail when open file\n");
@@ -60,16 +62,17 @@ void createGraph(char *inputFile, char *outputFile) {
         free(graph.adjacencyList[i]);
     free(graph.adjacencyList);
     free(graph.vertices);
+    free(graph.edgesCount);
     fclose(fp);
 }
 
-void addVertexAndEdgeToGraph(char ***adjacencyList, char **vertices, int **edgesCount, int *op,  const char *vertex,
-                              const char *edge) {
+void addVertexAndEdgeToGraph(char ***adjacencyList, char **vertices, int **edgesCount, int *op, const char *vertex,
+                             const char *edge) {
     int indexOfVertex = contains(*vertices, *vertex);
 
     if (indexOfVertex == -1) {
         *vertices = (char *) realloc(*vertices, (*op + 1) * sizeof(char));
-        *vertices[*op] = *vertex;
+        (*vertices)[*op] = *vertex;
 
 
         *adjacencyList = (char **) realloc(*adjacencyList, (*op + 1) * sizeof(char *));
@@ -79,22 +82,24 @@ void addVertexAndEdgeToGraph(char ***adjacencyList, char **vertices, int **edges
         }
 
         *edgesCount = (int *) realloc(*edgesCount, (*op + 1) * sizeof(int));
-        *edgesCount[*op] = 1;
+        (*edgesCount)[*op] = 1;
+        if (contains(*vertices, *edge) == -1) {
 
-        *adjacencyList[*op] = NULL;
-        *adjacencyList[*op] = (char *) realloc(*adjacencyList[*op], 1 * sizeof(char));
-        if (*adjacencyList[*op] == NULL) {
+        }
+        (*adjacencyList)[*op] = NULL;
+        (*adjacencyList)[*op] = (char *) realloc((*adjacencyList)[*op], 1 * sizeof(char));
+        if ((*adjacencyList)[*op] == NULL) {
             printf("Sequence %d is empty", *op);
             exit(1);
         }
-        *adjacencyList[*op][0] = *edge;
+        (*adjacencyList)[*op][0] = *edge;
         *op += 1;
-    } else {
-        *adjacencyList[indexOfVertex] = (char *)
-                realloc(*adjacencyList[indexOfVertex], (*edgesCount[indexOfVertex] + 1) * sizeof(char));
+    } else if (contains((*adjacencyList)[indexOfVertex], *edge) == -1) {
+        (*adjacencyList)[indexOfVertex] = (char *)
+                realloc((*adjacencyList)[indexOfVertex], ((*edgesCount)[indexOfVertex] + 1) * sizeof(char));
         if (adjacencyList[indexOfVertex] == NULL) exit(1);
-        *edgesCount[indexOfVertex] += 1;
-        *adjacencyList[indexOfVertex][*edgesCount[indexOfVertex] - 1] = *edge;
+        (*edgesCount)[indexOfVertex] += 1;
+        (*adjacencyList)[indexOfVertex][(*edgesCount)[indexOfVertex] - 1] = *edge;
     }
 }
 
